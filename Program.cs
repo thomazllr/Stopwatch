e3fdfdc57158e1c83133d0c89b3018e288ddfab4
Guilherme Thomaz <guithomaz270@gmail.com>
2023-03-13 23:03:30 -0300

    Stopwatch

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundos");
+            Console.WriteLine("M = Minutos");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
+
+            if(time == 0)
+                System.Environment.Exit(0);
+
+            PreStart(time * multiplier);
+        }
+
+        static void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("READY...");
+            Thread.Sleep(1000);
+            Console.WriteLine("SET...");
+            Thread.Sleep(1000);
+            Console.WriteLine("GO!!!!");
+            Thread.Sleep(2500);
+
+            Start(time); 
+
+        }
+
+        static void Start(int time)
+        {
+
+            int currentTime = 0;
+
+            while (currentTime != time)
+            {
+                Console.Clear();                
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear(); ;
+            Console.WriteLine("Stopwatch finalizado!");
+            Thread.Sleep(2000);
+            Menu();
+        }
+    }   
+}
